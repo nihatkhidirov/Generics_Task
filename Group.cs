9318514abd2c3b84f71cc79843f7e3df742986d3
nihatkhidirov <nihatmkh@code.edu.az>
2024-03-19 16:30:12 +0400

    Firs Commit

---
+++ Group.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Generics
+{
+    internal class Group:Students
+    {
+        private static int _No = 101;
+
+        public string No { get; }
+        public Group()
+        {
+            No = "AP" + _No.ToString();
+            _No++;
+        }
+
+        private List<Students> students;
+        public Group()
+        {
+            students = new List<Students>(); 
+        }
+        public List<Students> GetAllStudents()
+        {
+            return students;
+        }
+
+
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Generics
+{
+    internal class Students
+    {
+        public Students(string name, string surname, double point)
+        {
+            Name = name;
+            Surname = surname;
+            Point = point;
+        }
+        private static int _idCounter = 1;
+        public int Id { get; } 
+
+        public Students()
+        {
+            Id = _idCounter++; 
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Point { get; set; }
+        public int Age { get; set; }
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"ID: {Id}\n Name: {Name} \n Surname: {Surname}\n Age: {Age}\n Point: {Point}");
+        }
+    }
+}
